54c17cbb6d3c3c23fc3187a8f6ad2f0e1001a1dd
Ashise05 <152582134+Ashise05@users.noreply.github.com>
2023-12-01 11:00:23 +0600

    Update summation code.c

--- summation code.c
+++ summation code.c
@@ -4,9 +4,9 @@ int main()
 int a;
 int b;
 int sum;
-printf("Enter 1st number");
+printf("Enter 1st num:");
 scanf("%d", a);
-printf("Enter 2nd number");
+printf("Enter 2nd num:");
 scanf("%d", b);
 sum = a+b;
 printf("Sum= %d", sum);
